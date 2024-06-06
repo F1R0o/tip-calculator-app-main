e6c3e2459c4c0b9c646ee8721c893dc0b0595b95
luka <102388689+F1R0o@users.noreply.github.com>
2024-06-06 22:51:07 +0400

    new

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -1,3 +1,11 @@
+const billAmount = parseFloat(document.getElementById('bill').value);
+const tipPercentage = parseFloat(document.getElementById('tip-percentage').value);
+const numOfPeople = parseInt(document.getElementById('num-of-people').value);
+const resetButton = document.querySelector('.reset');
+const resultDiv = document.getElementById('result');
+    
+
+
 function setTip(percentage) {
     document.getElementById('tip-percentage').value = percentage;
 }
@@ -16,6 +24,17 @@ function calculateTip() {
     const totalAmount = billAmount + tipAmount;
     const amountPerPerson = totalAmount / numOfPeople;
     
+    
     document.getElementById('result').innerHTML = `<p>Tip Amount per Person: $${(tipAmount / numOfPeople).toFixed(2)}</p><p>Total Amount per Person: $${amountPerPerson.toFixed(2)}</p>`;
 
 }
+
+
+resetButton.addEventListener("click",function(){
+    resultDiv.innerHTML = '<p>Tip Amount per person: $0</p><p>Total Amount per person: $0</p>'
+    document.getElementById("bill").value = ''
+    document.getElementById("tip-percentage").value = ''
+    document.getElementById("num-of-people").value = ''
+})
+
+
